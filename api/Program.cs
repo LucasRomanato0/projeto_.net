b5b920358cc274247de6bd02a2cc421d8dcbc8c4
LucasRomanato0 <lucasromanato34@gmail.com>
2023-09-25 11:25:06 -0300

    criando end points

--- api/Program.cs
+++ api/Program.cs
@@ -2,5 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World 2!");
+app.MapPost("/", () => new { Name = "Lucas Romanato", Age = 22 });
+app.MapGet("/AddHeader", (HttpResponse response) =>
+{
+    response.Headers.Add("Teste", "Lucas Romanato");
+    return new { Name = "Lucas Romanato", Age = 22 };
+});
 
 app.Run();
